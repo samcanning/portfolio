9fe83300ed21c5437006975e7a3bb3df1cb4e842
Sam Canning <samcanning@outlook.com>
2018-04-18 14:22:28 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using portfolio.Models;
+
+namespace portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
